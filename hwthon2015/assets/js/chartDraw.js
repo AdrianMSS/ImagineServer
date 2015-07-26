b42a42699a029fba7897d10588157e34e9f0c304
AdrianMSS <sesamaua@gmail.com>
2015-07-26 04:36:45 -0600

    Commit#21 GETS Teams

--- hwthon2015/assets/js/chartDraw.js
+++ hwthon2015/assets/js/chartDraw.js
@@ -9,6 +9,7 @@ function getData(){
     beforeSend: setHeader,
     success: function(res){
       drawChart(res);
+      drawChart2(res);
     }
   });
 };
@@ -40,5 +41,33 @@ function drawChart(dataJson) {
   };
 
   var chart = new google.visualization.AreaChart(document.getElementById('chart_div'));
+  chart.draw(data, options);
+}
+
+function drawChart2(dataJson) {
+  var data = new google.visualization.DataTable();
+  data.addColumn('date', 'Fecha');
+  var teamsArray = ['ElectronicsSolutionDevices','Bulldozer','OvejasElectricas','SmarTicos','InnovationSourceCode',
+    'Envitech','Neotronic','InfotronicCircuits','Float','InDePro','LaNaranjaMecanica','FrozenbyteKnights','Iwa',
+    'TeamCR','NovaMakers','A','WIN','X','Y','Z'];
+
+  teamsArray.forEach(function(obj){
+    data.addColumn('number', obj);
+  });
+
+  _.each(dataJson[0].logs.get, function(obj, key){
+    data.addRow([new Date(key),  obj.ElectronicsSolutionDevices, obj.Bulldozer, obj.OvejasElectricas, obj.SmarTicos, 
+      obj.InnovationSourceCode, obj.Envitech, obj.Neotronic, obj.InfotronicCircuits, obj.Float, obj.InDePro, 
+      obj.LaNaranjaMecanica, obj.FrozenbyteKnights, obj.Iwa, obj.TeamCR, obj.NovaMakers, obj.A, obj.WIN, obj.X
+      , obj.Y, obj.Z]);
+  });
+
+  var options = {
+    title: 'Cantidad de GETS de los equipos',
+    hAxis: {title: 'Fecha',  titleTextStyle: {color: '#333'}},
+    vAxis: {minValue: 0}
+  };
+
+  var chart = new google.visualization.AreaChart(document.getElementById('chart_div2'));
   chart.draw(data, options);
 }
